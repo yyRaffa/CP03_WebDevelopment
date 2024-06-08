f0f6e2cc9993281aed7985a7c9f9a3b5c8aa35b3
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-08 19:32:29 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
 const form = document.getElementById('form');
+
 const formCampos = document.querySelectorAll('.input-box');
+
 const telCampos = document.querySelectorAll('.input-telefone-box');
-const textArea = document.getElementById('textarea-box');
+const adicionarTel = document.getElementById('adicionar-telefone');
+const excluirTel = document.getElementById('excluir-telefone');
+
+const perfilCampo = document.getElementById('select-perfil-box');
+const textAreaCampo = document.getElementById('textarea-box');
+
+
+document.getElementById('textarea-box').addEventListener('blur', textareaMaiuscula);
+
+function textareaMaiuscula() {
+    var resultado;
+    resultado = textAreaCampo.value.toUpperCase();
+    textAreaCampo.value = resultado;
+}
