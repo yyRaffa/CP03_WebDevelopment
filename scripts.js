6dde5e51e3b3fdf38b5aecf856e6138cf8b970f9
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-08 19:04:12 -0300

    Update

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const formCampos = document.querySelectorAll('.input-box');
+const telCampos = document.querySelectorAll('.input-telefone-box');
+const textArea = document.getElementById('textarea-box');
