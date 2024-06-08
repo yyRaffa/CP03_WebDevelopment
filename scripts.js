5bcd5d8922d4bb2a31dc7db048f58430714c1418
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-08 19:51:58 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form');
 
 const formCampos = document.querySelectorAll('.input-box');
+const spans = document.querySelectorAll('.span-required');
 
 const telCampos = document.querySelectorAll('.input-telefone-box');
 const adicionarTel = document.getElementById('adicionar-telefone');
@@ -10,6 +11,48 @@ const perfilCampo = document.getElementById('select-perfil-box');
 const textAreaCampo = document.getElementById('textarea-box');
 
 
+// Validações
+function setErro(i) {
+    formCampos[i].style.border = '2px solid #e63636';
+    spans[i].style.display = 'flex';
+}
+
+function removeErro(i) {
+    formCampos[i].style.border = '';
+    spans[i].style.display = 'none';
+}
+
+function validarLogin() {
+    const validacao = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if(!validacao.test(formCampos[0].value))
+        setErro(0);
+    else
+        removeErro(0);
+}
+
+function validarSenha() {
+    if(formCampos[1].value.length < 8)
+        setErro(1);
+    else
+        removeErro(1);
+        validarConfirmarSenha();
+}
+
+function validarConfirmarSenha() {
+    if(formCampos[1].value == formCampos[2].value && formCampos[2].value.length >= 8)
+        removeErro(2);
+    else
+        setErro(2);
+}
+
+function validarNome() {
+    if(formCampos[3].value.length < 3)
+        setErro(3);
+    else
+        removeErro(3);
+}
+
+// Convertendo textos
 document.getElementById('textarea-box').addEventListener('blur', textareaMaiuscula);
 
 function textareaMaiuscula() {
@@ -17,3 +60,4 @@ function textareaMaiuscula() {
     resultado = textAreaCampo.value.toUpperCase();
     textAreaCampo.value = resultado;
 }
+
