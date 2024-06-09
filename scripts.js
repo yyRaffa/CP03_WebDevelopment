21f1fdfbb6e8d28efbdbfb94fa92738b5409023e
Rafael Oliveira <yraffa.olive@gmail.com>
2024-06-08 21:24:52 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -3,15 +3,36 @@ const form = document.getElementById('form');
 const formCampos = document.querySelectorAll('.input-box');
 const spans = document.querySelectorAll('.span-required');
 
+const formTel = document.getElementById('telefone-inputs');
 const telCampos = document.querySelectorAll('.input-telefone-box');
-const adicionarTel = document.getElementById('adicionar-telefone');
-const excluirTel = document.getElementById('excluir-telefone');
+const adicionarTelBotao = document.getElementById('adicionar-telefone');
+const excluirTelBotao = document.getElementById('excluir-telefone');
 
 const perfilCampo = document.getElementById('select-perfil-box');
 const textAreaCampo = document.getElementById('textarea-box');
 
 
 // Validações
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    validarLogin();
+    validarSenha();
+    validarConfirmarSenha();
+    validarNome();
+    validarCPF();
+    validarVoucher();
+});
+
+window.addEventListener('load', (event) => {
+    event.preventDefault();
+    validarLogin();
+    validarSenha();
+    validarConfirmarSenha();
+    validarNome();
+    validarCPF();
+    validarVoucher();
+});
+
 function setErro(i) {
     formCampos[i].style.border = '2px solid #e63636';
     spans[i].style.display = 'flex';
@@ -33,9 +54,10 @@ function validarLogin() {
 function validarSenha() {
     if(formCampos[1].value.length < 8)
         setErro(1);
-    else
+    else {
         removeErro(1);
         validarConfirmarSenha();
+    }
 }
 
 function validarConfirmarSenha() {
@@ -52,12 +74,60 @@ function validarNome() {
         removeErro(3);
 }
 
+function validarCPF() {
+    const validacao = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    if(!validacao.test(formCampos[5].value))
+        setErro(5);
+    else
+        removeErro(5);
+}
+
+function validarVoucher() {
+    const voucherPermitidos = {
+        'A': 'PRATA123',
+        'B': 'OURO123',
+        'C': 'DIAMANTE123'
+    };
+
+    if(formCampos[6].value === voucherPermitidos[perfilCampo.value])
+        removeErro(6);
+    else
+        setErro(6);
+}
+
+
+// Formulário de Telefones
+let contadorTel = 1;
+const maxTel = 3;
+const minTel = 1;
+
+function adicionarTel() {
+    if(contadorTel < maxTel) {
+        contadorTel++;
+        const inputTelefoneBox = document.createElement('div');
+        inputTelefoneBox.classList.add('input-telefone');
+        inputTelefoneBox.innerHTML = '<input class="input-telefone-box" id="telefone" type="tel" name="telefone" placeholder="xxxxx-xxxx">';
+        formTel.insertBefore(inputTelefoneBox, adicionarTelBotao);
+    }
+}
+
+function excluirTel() {
+
+}
+
+
 // Convertendo textos
+document.getElementById('voucher').addEventListener('input', voucherMaiuscula);
 document.getElementById('textarea-box').addEventListener('blur', textareaMaiuscula);
+
+function voucherMaiuscula() {
+    var resultado;
+    resultado = formCampos[6].value.toUpperCase();
+    formCampos[6].value = resultado;
+}
 
 function textareaMaiuscula() {
     var resultado;
     resultado = textAreaCampo.value.toUpperCase();
     textAreaCampo.value = resultado;
 }
-
